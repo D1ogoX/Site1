0f7f8a0f72632a47c75c0dbea374e469155e3be0
Diogo Santiago <diogosantiago@forbrand.pt>
2021-09-09 11:08:09 +0100

    Add project files.

---
+++ SiteExemplo1/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteExemplo1.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        // GET: Produtos
+        public ActionResult Vips()
+        {
+            return View();
+        }
+    }
+}
